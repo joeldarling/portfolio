72695320fc0d919911e1844b43ea0a5fc6edb137
Joel <joel.darling@gmail.com>
2016-09-26 18:45:19 -0400

    updated bio

--- app/components/Bio.jsx
+++ app/components/Bio.jsx
@@ -1,13 +1,15 @@
 import React, {Component, PropTypes} from 'react';
 
+import Social from 'Social';
+
 export default class Bio extends Component {
 	render() {
 
 		return (
       <div>
         <h4> <i className="fa fa-code"></i> Software Engineer </h4>
-				<h5> <i className="fa fa-map-marker"></i> NYC. </h5>
-        <p> Fullstack Web Developer, and also <a href="http://joeldarling.com" target="_blank">musician</a></p>
+				<h5> <i className="fa fa-map-marker"></i> NYC </h5>
+				<Social/>
       </div>
     );
 	}
---
+++ app/components/Social.jsx
@@ -0,0 +1,16 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class Social extends Component {
+	render() {
+
+		return (
+      <div className="social-bar">
+        <ul>
+          <li><a href="https://github.com/joeldarling" target="_blank"><i className="fa fa-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/joeldarling" target="_blank"><i className="fa fa-linkedin-square"></i></a></li>
+          <li><a href="" target="_blank"><i className="fa fa-file-pdf-o"></i></a></li>
+        </ul>
+      </div>
+    );
+	}
+}
