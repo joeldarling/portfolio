f6699d2d3c79e4a16b532e1793fdb442f361cc2f
Joel <joel.darling@gmail.com>
2016-10-07 15:43:58 -0400

    install trace

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require('express');
+require('@risingstack/trace'); // trace should be on top
+
+const express = require('express');
 const path = require('path');
 
 // Create our app
@@ -6,19 +8,19 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
+	if (req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
 });
 
 app.use(express.static('public'));
 
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+	response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
+	console.log('Express server is up on port ' + PORT);
 });
---
+++ trace.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	serviceName: 'Server',
+	apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3ZjdmOGFmYmRmNjZkMDAxMTFhZjE1YyIsImlhdCI6MTQ3NTg2ODg0OH0.jVmDo5rq-c0vsD4obLxDmKhztP_Jdr4EX5dYnIuG_i4'
+};
